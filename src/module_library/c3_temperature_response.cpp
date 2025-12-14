806d2ca05b7c397e397a4c71b62e75e512606e87
sruthi162114001 <162114001@smail.iitpkd.ac.in>
2025-12-14 12:44:55 +0530

    corrections made for line 54

--- src/module_library/c3_temperature_response.cpp
+++ src/module_library/c3_temperature_response.cpp
@@ -51,12 +51,12 @@ c3_param_at_tleaf c3_temperature_response(
 
     return c3_param_at_tleaf{
         /* Gstar =      */ arrhenius_exponential(param.Gstar_c, param.Gstar_Ea, Tleaf_K),
-        /* Jmax_norm =  */ johnson_eyring_williams_response(param.Jmax_c, param.Jmax_Ha, param.Jmax_Hd, param.Jmax_S,Tleaf_K)};
+        /* Jmax_norm =  */ johnson_eyring_williams_response(param.Jmax_c, param.Jmax_Ha, param.Jmax_Hd, param.Jmax_S, Tleaf_K),
         /* Kc =         */ arrhenius_exponential(param.Kc_c, param.Kc_Ea, Tleaf_K),
         /* Ko =         */ arrhenius_exponential(param.Ko_c, param.Ko_Ea, Tleaf_K),
         /* phi_PSII =   */ polynomial_response(param.phi_PSII_0, param.phi_PSII_1, param.phi_PSII_2, Tleaf),
         /* RL_norm =    */ arrhenius_exponential(param.RL_c, param.RL_Ea, Tleaf_K),
         /* theta =      */ polynomial_response(param.theta_0, param.theta_1, param.theta_2, Tleaf),
         /* Tp_norm =    */ johnson_eyring_williams_response(param.Tp_c, param.Tp_Ha, param.Tp_Hd, param.Tp_S, Tleaf_K),
-        /* Vcmax_norm = */ johnson_eyring_williams_response(param.Vcmax_c, param.Vcmax_Ha, param.Vcmax_Hd, param.Vcmax_S,Tleaf_K)};
+        /* Vcmax_norm = */ johnson_eyring_williams_response(param.Vcmax_c, param.Vcmax_Ha, param.Vcmax_Hd, param.Vcmax_S, Tleaf_K)};
 }
